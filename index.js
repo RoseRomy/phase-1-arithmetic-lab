99d4d0d480f92e642ea74879e3fca25d1c2972b7
+ <RoseKamau@Romys-Macboook-Air.local>
2025-03-04 10:21:59 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Define two numbers whose product is 62
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define two numbers whose remainder is 4
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4; // 10 % 6 = 4
+
+// Find the highest number in a set
+const max = Math.max(5, 10, 15, 20); // 20 is the highest number
